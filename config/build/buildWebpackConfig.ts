d758f2f8790120cc88ad89bb57dedadafa890922
andrei110295@yandex.ru <andrei110295@yandex.ru>
2023-08-19 14:37:44 +0300

    added fsd

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -19,7 +19,7 @@ export const buildWebpackConfig = function(options: BuildOptions): webpack.Confi
     module: {
       rules: buildLoader(options),
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev ? 'inline-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   }
